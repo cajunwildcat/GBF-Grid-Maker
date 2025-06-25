08b2b73c425d904cd634b15f24825afd1e90be58
Eli Cheramie <elicheramie89@gmail.com>
2025-06-25 13:21:18 -0400

    added search weights system

--- main.js
+++ main.js
@@ -1,171 +1,7 @@
 let cOptions = [];
 let wOptions = [];
 let sOptions = [];
-let clOptions = [
-    { label: "Berserker", metatags: ["100301_sw"] },
-    { label: "Viking", metatags: ["100401_sw"] },
-    { label: "Spartan", metatags: ["110301_sw"] },
-    { label: "Paladin", metatags: ["110401_sw"] },
-    { label: "Sage", metatags: ["120301_wa"] },
-    { label: "Iatromantis", metatags: ["120401_wa"] },
-    { label: "Warlock", metatags: ["130301_wa"] },
-    { label: "Manadiver", metatags: ["130401_wa"] },
-    { label: "Bandit Tycoon", metatags: ["140301_kn"] },
-    { label: "Street King", metatags: ["140401_kn"] },
-    { label: "Chaos Ruler", metatags: ["150301_sw"] },
-    { label: "Onmyoji", metatags: ["150401_kn"] },
-    { label: "Luchador", metatags: ["160301_me"] },
-    { label: "Sumaibito", metatags: ["160401_me"] },
-    { label: "Nighthound", metatags: ["170301_bw"] },
-    { label: "Boogeyman", metatags: ["170401_bw"] },
-    { label: "Elysian", metatags: ["180301_mc"] },
-    { label: "Mariachi", metatags: ["180401_mc"] },
-    { label: "Apsaras", metatags: ["190301_sp"] },
-    { label: "Alchemist", metatags: ["200201_kn"] },
-    { label: "Doctor", metatags: ["200301_kn"] },
-    { label: "Ninja", metatags: ["210201_kt"] },
-    { label: "Runeslayer", metatags: ["210301_kt"] },
-    { label: "Samurai", metatags: ["220201_kt"] },
-    { label: "Kengo", metatags: ["220301_kt"] },
-    { label: "Sword Master", metatags: ["230201_sw"] },
-    { label: "Glorybringer", metatags: ["230301_sw"] },
-    { label: "Gunslinger", metatags: ["240201_gu"] },
-    { label: "Soldier", metatags: ["240301_gu"] },
-    { label: "Mystic", metatags: ["250201_wa"] },
-    { label: "Nekomancer", metatags: ["250301_wa"] },
-    { label: "Assassin", metatags: ["260201_kn"] },
-    { label: "Tormentor", metatags: ["260301_kn"] },
-    { label: "Drum Master", metatags: ["270201_mc"] },
-    { label: "Rising Force", metatags: ["270301_mc"] },
-    { label: "Dancer", metatags: ["280201_kn"] },
-    { label: "Masquerade", metatags: ["280301_kn"] },
-    { label: "Mechanic", metatags: ["290201_gu"] },
-    { label: "Chrysaor", metatags: ["300301_sw"] },
-    { label: "Lumberjack", metatags: ["410301_ax"] },
-    { label: "Cavalier", metatags: ["420301_sp"] },
-    { label: "Monk", metatags: ["430301_me"] },
-    { label: "Robin Hood", metatags: ["440301_bw"] },
-    { label: "Relic Buster", metatags: ["450301_sw"] },
-    { label: "Yamato", metatags: ["460301_sw"] },
-    { label: "Shieldsworn", metatags: ["470301_ax"] }
-];
 let aOptoins = [];
-let opusSkillOptions = {
-    skill2: [
-        { label: "α Revelation", metatags: ["auto", "α Revelation", "normal", "alpha"], img: "https://gbf.wiki/images/thumb/f/f6/Ws_skill_normal_limit.png/50px-Ws_skill_normal_limit.png" },
-        { label: "β Revelation", metatags: ["skill", "β Revelation", "beta"], img: "https://gbf.wiki/images/thumb/6/60/Ws_skill_ability_limit.png/50px-Ws_skill_ability_limit.png" },
-        { label: "γ Revelation", metatags: ["ca", "γ Revelation", "gamma", "ougi", "charge attack"], img: "https://gbf.wiki/images/thumb/b/b7/Ws_skill_special_limit.png/50px-Ws_skill_special_limit.png" },
-        { label: "Δ Revelation", metatags: ["cb", "Δ Revelation", "delta", "chain burst"], img: "https://gbf.wiki/images/thumb/1/13/Ws_skill_chain_limit.png/50px-Ws_skill_chain_limit.png" },
-    ],
-    skill3: [
-        { label: "Stamina", metatags: ["stamina", "strength"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_whole_m_<element>_3.png" },
-        { label: "Emnity", metatags: ["emnity", "zeal"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_backwater_m_<element>_3.png" },
-        { label: "Trium", metatags: ["strife", "trium"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_ta_m_<element>_2.png" },
-        { label: "Progression", metatags: ["progression", "prosperity"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/14004.jpg" },
-        { label: "Celere", metatags: ["celere", "temperament"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_moment_m_<element>.png" },
-        { label: "Majesty", metatags: ["majesty", "restoration"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_god_m_<element>_2.png" },
-        { label: "Glory", metatags: ["glory", "glorification"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_hero_m_<element>_3.png" },
-        { label: "Cunning Tempation", metatags: ["freyr", "temptation"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/14014.jpg" },
-        { label: "Forbidden Fruit", metatags: ["apple", "fruit", "forbiddance"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/14015.jpg" },
-        { label: "Wicked Conduct", metatags: ["conduct", "depravity"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/14016.jpg" },
-        { label: "Deceitful Fallacy", metatags: ["echo", "fallacy", "falsehood"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/14017.jpg" },
-        { label: "Divine Ruination", metatags: ["extremity", "ruination"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/14005.jpg" },
-        { label: "Divine Honing", metatags: ["sagacity", "honing"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/14006.jpg" },
-        { label: "Divine Fathoms", metatags: ["supremacy", "fathoms"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/14007.jpg" },
-    ]
-};
-let ccwSkillOptions = {
-    skill2: [
-        { label: "Humanity", metatags: ["humanity"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/12001.jpg" },
-        { label: "Divinity", metatags: ["divinity"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/12002.jpg" },
-        { label: "Devilry", metatags: ["devilry"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/12003.jpg" },
-    ]
-}
-let ultimaSkillOptions = {
-    skill1: [
-        { label: "Guaph Key of Will", metatags: ["atk", "will", "dominion"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_weapon_atk_<weapon type>.png" },
-        { label: "Guaph Key of Strife", metatags: ["ma", "strife", "parity"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_weapon_da_<weapon type>.png" },
-        { label: "Guaph Key of Vitality", metatags: ["hp", "vitality", "utopia"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_weapon_hp_<weapon type>.png" },
-        { label: "Guaph Key of Strength", metatags: ["stamina", "strength", "plenum"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_weapon_whole_<weapon type>.png" },
-        { label: "Guaph Key of Zeal", metatags: ["enmity", "zeal", "ultio"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_weapon_backwater_<weapon type>.png" },
-        { label: "Guaph Key of Courage", metatags: ["crit", "courage", "ars"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_weapon_tech_<weapon type>.png" },
-    ],
-    skill2: [
-        { label: "Guaph Key α", metatags: ["auto", "Guaph Key α", "normal", "alpha"], img: "https://gbf.wiki/images/thumb/f/f6/Ws_skill_normal_limit.png/50px-Ws_skill_normal_limit.png" },
-        { label: "Guaph Key β", metatags: ["skill", "Guaph Key β", "beta"], img: "https://gbf.wiki/images/thumb/6/60/Ws_skill_ability_limit.png/50px-Ws_skill_ability_limit.png" },
-        { label: "Guaph Key γ", metatags: ["ca", "Guaph Key γ", "gamma", "ougi", "charge attack"], img: "https://gbf.wiki/images/thumb/b/b7/Ws_skill_special_limit.png/50px-Ws_skill_special_limit.png" },
-        { label: "Guaph Key Δ", metatags: ["cb", "Guaph Key Δ", "delta", "chain burst"], img: "https://gbf.wiki/images/thumb/1/13/Ws_skill_chain_limit.png/50px-Ws_skill_chain_limit.png" },
-    ],
-    skill3: [
-        { label: "Guaph Key Ena", metatags: ["cap", "ena", "fortis"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/17001.jpg" },
-        { label: "Guaph Key Dio", metatags: ["healing", "dio", "sanatio"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/17002.jpg" },
-        { label: "Guaph Key Tria", metatags: ["seraphic", "tria", "impetus"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/17003.jpg" },
-        { label: "Guaph Key Tessera", metatags: ["cbgain", "tessera", "elatio"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/17004.jpg" },
-    ]
-}
-let draconicSkillOptions = {
-    skill2: [
-        { label: "Endurance Teluma", metatags: ["def", "endurance", "true dragon"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_damage_red_0.png" },
-        { label: "Inferno Teluma", metatags: ["fire", "inferno", "vermillion"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_damage_red_1.png" },
-        { label: "Abyss Teluma", metatags: ["water", "abyss", "azure"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_damage_red_2.png" },
-        { label: "Crag Teluma", metatags: ["earth", "crag", "golden"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_damage_red_3.png" },
-        { label: "Tempest Teluma", metatags: ["wind", "tempest", "emerald"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_damage_red_4.png" },
-        { label: "Aureole Teluma", metatags: ["light", "aureole", "white"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_damage_red_5.png" },
-        { label: "Malice Teluma", metatags: ["dark", "malice", "black"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_damage_red_6.png" },
-        { label: "Salvation Teluma", metatags: ["hp", "salvation", "fortitude"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_draconic_hp_add_5.png" },
-        { label: "Oblivion Teluma", metatags: ["amp", "oblivion", "magnitude"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/assets/item/skillplus/s/15009.jpg" },
-    ],
-    skill3: [
-        { label: "Optimus Teluma", metatags: ["primal", "optimus"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_god_<element>_3.png" },
-        { label: "Omega Teluma", metatags: ["magna", "omega"], img: "https://prd-game-a-granbluefantasy.akamaized.net/assets_en/img/sp/ui/icon/skill/skill_god_m_<element>_3.png" },
-
-    ]
-}
-let minoOptions = [
-    { label: "Burlona", metatags: ["1", "burlona"] },
-    { label: "Schalk", metatags: ["2", "schalk"] },
-    { label: "Leviathan Mino", metatags: ["3", "levi"] },
-    { label: "Yggdrasil Mino", metatags: ["4", "yggy"] },
-    { label: "Bahamut", metatags: ["5", "baha"] },
-    { label: "Lu Woh Mino", metatags: ["6", "luwoh"] },
-    { label: "Mini Mimic", metatags: ["7", "mimic"] },
-    { label: "Ouroboros", metatags: ["8", "ouro", "cag"] },
-    { label: "Europa", metatags: ["9", "europa"] },
-    { label: "Wilnas", metatags: ["10", "wilnas"] },
-    { label: "Agastia Simulacrum", metatags: ["11", "agastia", "agatura"] },
-    { label: "Dark Rapture Simulacrum", metatags: ["12", "faa", "blue"] },
-    { label: "Chachazero", metatags: ["13", "chachazero", "evangeline"] },
-    { label: "Perfida", metatags: ["14", "ccw"] }
-]
-let shieldOptions = [
-    { label: "Round Shield", metatags: ["2001", "round shield", "round", "hp1"] },
-    { label: "Buckler", metatags: ["2002", "buckler", "dodge"] },
-    { label: "Knight Shield", metatags: ["2003", "knight shield", "knight", "def1"] },
-    { label: "Scutum", metatags: ["3001", "scutum", "def2"] },
-    { label: "Mythril Shield", metatags: ["3002", "mythril shield", "mythril", "hp2"] },
-    { label: "Holy Shield", metatags: ["3003", "holy shield", "holy", "darkres"] },
-    { label: "Tiamat Shield", metatags: ["3004", "tiamat shield", "tiamat", "windatk"] },
-    { label: "Rose Crystal Shield", metatags: ["3005", "rose crystal shield", "rose", "superiorelementres"] },
-    { label: "Spartan Shield", metatags: ["4001", "spartan shield", "spartan", "default", "def3"] },
-    { label: "Malice Adarga", metatags: ["4002", "malice adarga", "malice", "atk"] },
-    { label: "Archangel's Shield", metatags: ["4003", "archangel's shield", "angel", "hp3"] },
-    { label: "Colossus Wall", metatags: ["4004", "colossus wall", "colossus", "fireatk"] },
-    { label: "Bahamut Shield", metatags: ["4005", "bahamut shield", "bahamut", "baha", "cadmg"] },
-    { label: "Soul of Oneness", metatags: ["4006", "soul of oneness", "twinelements", "firewaterdef"] },
-    { label: "Eutr Nogadr Ldeysh", metatags: ["4007", "eutr nogadr ldeysh", "lindwurm", "debuffres"] },
-    { label: "Hero's Shield", metatags: ["4008", "hero's shield", "hero", "gilgamesh", "gilg", "def"] },
-    { label: "Shield of Lamentation", metatags: ["4009", "shield of lamentation", "grandorder", "go", "nadmg"] },
-    { label: "Huanglong Shield", metatags: ["4010", "huanglong shield", "huanglong", "lightatk"] },
-    { label: "Qilin Shield", metatags: ["4011", "qilin shield", "qilin", "darkatk"] },
-    { label: "Nibelung Mauer", metatags: ["4012", "nibelung mauer", "alex", "alexiel", "waterres"] },
-    { label: "Obelisk", metatags: ["4013", "obelisk", "ennead", "cb"] },
-    { label: "Shield of the Enthroned", metatags: ["4014", "atum", "enthroned", "shield of the enthroned"] },
-    { label: "Lustrous Wall", metatags: ["4015", "horus", "lustrous wall"] },
-    { label: "Eth Ldog Ldeysh", metatags: ["4016", "galleon", "eth ldog ldeysh"] },
-    { label: "Eth Ckalb Ldeysh", metatags: ["4017", "fediel", "eth ckalb ldeysh"] },
-    { label: "Moonhill", metatags: ["4018", "dias", "diaspora", "moonhill"] },
-    { label: "Shield of Tenets", metatags: ["4019", "subhl", "subbie", "tenets", "shield of tenets"] }
-]
 let elements = { "fire": 1, "water": 2, "earth": 3, "wind": 4, "light": 5, "dark": 6 };
 let weaponTypes = { "sabre": 1, "dagger": 2, "spear": 3, "axe": 4, "staff": 5, "gun": 6, "melee": 7, "bow": 8, "harp": 9, "katana": 10 };
 let worldHarps = [1040815000, 1040815100, 1040815200, 1040815300, 1040815400];
@@ -209,6 +45,7 @@ window.onload = async (e) => {
         let c = characters[id];
         let name = c.pageName;
         characterIDs[name] = id;
+
         let metas = [id.toString()];
         c.jpname ? metas.push(c.jpname) : null;
         if (c.series && c.series.toLowerCase().includes("grand")) {
@@ -232,17 +69,30 @@ window.onload = async (e) => {
         if (alias = (aliases[name] || aliases[name.split(' (')[0]] || aliases[id])) {
             metas.push(...alias);
         }
+
+        let weight = 0;
+        if (weights[name]) weight = weights[name];
+
         cOptions.push({
             label: name,
-            metatags: metas
+            metatags: metas,
+            weight: weight
         });
     }
     for (let id in summons) {
         let name = summons[id].pageName;
         summonIDs[name] = id;
+
+        let weight = 0;
+        if (weights[name]) weight = weights[name];
+        else if (summons[id].series == "omega") weight = 1;
+        else if (summons[id].series == "optimus") weight = 1;
+        else if (summons[id].series == "collab") weight = -1;
+
         sOptions.push({
             label: name,
-            metatags: [id]
+            metatags: [id],
+            weight: weight
         });
         if (alias = (aliases[name] || aliases[name.split(' (')[0]] || aliases[id])) {
             sOptions[sOptions.length - 1].metatags.push(...alias);
@@ -251,9 +101,16 @@ window.onload = async (e) => {
     for (let id in weapons) {
         let name = weapons[id].pageName;
         weaponIDs[name] = id;
+
+        let weight = 0;
+        if (weights[name]) weight = weights[name];
+        else if (weapons[id].series == "dark opus") weight = 10;
+        else if (weapons[id].series == "collab") weight = -1;
+
         wOptions.push({
             label: name,
-            metatags: [id]
+            metatags: [id],
+            weight: weight
         });
         if (alias = (aliases[name] || aliases[name.split(' (')[0]] || aliases[id])) {
             wOptions[wOptions.length - 1].metatags.push(...alias);
@@ -267,7 +124,8 @@ window.onload = async (e) => {
         let name = ability;
         aOptoins.push({
             label: name,
-            metatags: [ability]
+            metatags: [ability],
+            weight: weights[name]? weights[name] : 0
         });
         if (alias = (aliases[name])) {
             aOptoins[aOptoins.length - 1].metatags.push(...alias);
@@ -494,7 +352,7 @@ function gridInputClick(event, sort = true, button = null) {
         }
     });
     if (sort) {
-        options = options.sort((a, b) => a.label.localeCompare(b.label));
+        options = options.sort((a, b) => b.weight - a.weight || a.label.localeCompare(b.label));
     }
     if (dropdown.style.display === 'block' && activeButton === button) {
         hideDropdown();
